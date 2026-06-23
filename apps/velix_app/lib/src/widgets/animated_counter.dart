c116d4068fea00ba21429429330b2fd7f9196c86
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:41:18 +0530

    feat: add animated counter widget for stats

---
+++ apps/velix_app/lib/src/widgets/animated_counter.dart
@@ -0,0 +1,8 @@
+﻿import 'package:flutter/material.dart';
+class AnimatedCounter extends StatelessWidget {
+  final int value;
+  final TextStyle? style;
+  const AnimatedCounter({super.key, required this.value, this.style});
+  @override
+  Widget build(BuildContext context) => TweenAnimationBuilder<int>(tween: IntTween(begin: 0, end: value), duration: const Duration(milliseconds: 800), curve: Curves.easeOut, builder: (_, v, __) => Text('\', style: style));
+}
