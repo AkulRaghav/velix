4d82d58bcf1263aacb25b87ec5370dd93843109a
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:41:18 +0530

    feat: add online status dot indicator

---
+++ apps/velix_app/lib/src/widgets/status_dot.dart
@@ -0,0 +1,10 @@
+﻿import 'package:flutter/material.dart';
+enum StatusDotState { online, away, offline, dnd }
+class StatusDot extends StatelessWidget {
+  final StatusDotState state;
+  final double size;
+  const StatusDot({super.key, required this.state, this.size = 10});
+  @override
+  Widget build(BuildContext context) => Container(width: size, height: size, decoration: BoxDecoration(shape: BoxShape.circle, color: _color, border: Border.all(color: Colors.black, width: 2)));
+  Color get _color => switch (state) { StatusDotState.online => Colors.green, StatusDotState.away => Colors.orange, StatusDotState.dnd => Colors.red, StatusDotState.offline => Colors.grey };
+}
