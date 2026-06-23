afe77b4a00b26b826aac1827d2914680785b0488
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:40:36 +0530

    feat: add platform detection utilities

---
+++ apps/velix_app/lib/src/utils/platform_utils.dart
@@ -0,0 +1,9 @@
+﻿import 'dart:io';
+class PlatformUtils {
+  static bool get isAndroid => Platform.isAndroid;
+  static bool get isIOS => Platform.isIOS;
+  static bool get isMobile => isAndroid || isIOS;
+  static bool get isDesktop => Platform.isWindows || Platform.isMacOS || Platform.isLinux;
+  static String get osName => Platform.operatingSystem;
+  static String get osVersion => Platform.operatingSystemVersion;
+}
