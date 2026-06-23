3873f54d2475ef90d5ac914f21b181c4faefa34e
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:41:18 +0530

    feat: add custom pull-to-refresh indicator

---
+++ apps/velix_app/lib/src/widgets/pull_to_refresh.dart
@@ -0,0 +1,8 @@
+﻿import 'package:flutter/material.dart';
+class VelixRefreshIndicator extends StatelessWidget {
+  final Widget child;
+  final Future<void> Function() onRefresh;
+  const VelixRefreshIndicator({super.key, required this.child, required this.onRefresh});
+  @override
+  Widget build(BuildContext context) => RefreshIndicator(onRefresh: onRefresh, color: const Color(0xFF3478F6), backgroundColor: const Color(0xFF1A1A24), child: child);
+}
