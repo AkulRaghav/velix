8eb50b6b7bdce4bcf838094e303372af00709d73
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:40:36 +0530

    feat: add online status model

---
+++ apps/velix_app/lib/src/models/online_status.dart
@@ -0,0 +1,8 @@
+﻿enum OnlineState { online, away, offline, doNotDisturb }
+
+class OnlineStatus {
+  final OnlineState state;
+  final DateTime? lastSeen;
+  const OnlineStatus({required this.state, this.lastSeen});
+  String get displayText => switch (state) { OnlineState.online => 'Online', OnlineState.away => 'Away', OnlineState.doNotDisturb => 'Do not disturb', OnlineState.offline => lastSeen != null ? 'Last seen recently' : 'Offline' };
+}
