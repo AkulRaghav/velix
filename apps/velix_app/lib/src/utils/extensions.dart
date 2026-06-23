4e9df56b11fbd09e46957124e33d920cd2d59179
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:40:01 +0530

    feat: add String and List extensions

---
+++ apps/velix_app/lib/src/utils/extensions.dart
@@ -0,0 +1,8 @@
+﻿extension StringX on String {
+  String get initials => split(' ').where((w) => w.isNotEmpty).map((w) => w[0].toUpperCase()).take(2).join();
+  String get truncated => length > 50 ? '${substring(0, 47)}...' : this;
+}
+
+extension ListX<T> on List<T> {
+  List<T> get unique => toSet().toList();
+}
