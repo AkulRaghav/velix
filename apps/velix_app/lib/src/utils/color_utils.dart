1f11a0b0d958474cecb01405e11fa8bfe34fcb52
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:40:36 +0530

    feat: add color manipulation utilities

---
+++ apps/velix_app/lib/src/utils/color_utils.dart
@@ -0,0 +1,9 @@
+﻿import 'package:flutter/material.dart';
+class ColorUtils {
+  static Color fromHash(String text) {
+    final hash = text.hashCode;
+    return Color.fromRGBO((hash & 0xFF0000) >> 16, (hash & 0x00FF00) >> 8, hash & 0x0000FF, 1.0);
+  }
+  static Color darken(Color c, [double amount = 0.1]) => Color.lerp(c, Colors.black, amount)!;
+  static Color lighten(Color c, [double amount = 0.1]) => Color.lerp(c, Colors.white, amount)!;
+}
