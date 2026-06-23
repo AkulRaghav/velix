c015a07afbff823e187e3e66446d522a4c3ebe1b
AkulRaghav <your-github-email@gmail.com>
2026-06-24 00:41:18 +0530

    feat: add shimmer loading placeholder widget

---
+++ apps/velix_app/lib/src/widgets/shimmer_loading.dart
@@ -0,0 +1,10 @@
+﻿import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+class ShimmerLoading extends StatelessWidget {
+  final double width;
+  final double height;
+  final double radius;
+  const ShimmerLoading({super.key, this.width = double.infinity, required this.height, this.radius = 8});
+  @override
+  Widget build(BuildContext context) => Shimmer.fromColors(baseColor: Colors.white.withValues(alpha: 0.05), highlightColor: Colors.white.withValues(alpha: 0.1), child: Container(width: width, height: height, decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(radius))));
+}
